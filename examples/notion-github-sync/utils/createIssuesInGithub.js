331964cb339522454b46838cf14312d65b358dcb
Fernando <fernando.ek@gmail.com>
2021-10-06 10:07:21 -0300

    Improving integration between Notion and Github: added state update

--- examples/notion-github-sync/utils/createIssuesInGithub.js
+++ examples/notion-github-sync/utils/createIssuesInGithub.js
@@ -15,42 +15,40 @@ module.exports = async function createIssuesInGithub(tasks) {
     const asignees = getAssigneesFromNotionTask(task)
     const labels = getLabelsFromNotionTask(task)
     let issueResult
-    // try {
-    //   issueResult = await octokit.rest.issues.create({
-    //     owner: process.env.GITHUB_REPO_OWNER,
-    //     repo: process.env.GITHUB_REPO_NAME,
-    //     title: task.properties.Name.title[0].plain_text,
-    //     body: "id: " + task.id + "\r\n url: " + task.url,
-    //     assignees: asignees,
-    //     labels: labels,
-    //   })
-    //   console.log("created issue", issueResult)
-    // } catch (e) {
-    //   console.log("error creating issue", e)
-    // }
+    try {
+      issueResult = await octokit.rest.issues.create({
+        owner: process.env.GITHUB_REPO_OWNER,
+        repo: process.env.GITHUB_REPO_NAME,
+        title: task.properties.Name.title[0].plain_text,
+        body: "id: " + task.id + "\r\n url: " + task.url,
+        assignees: asignees,
+        labels: labels,
+      })
+      console.log("created issue", issueResult)
+    } catch (e) {
+      console.log("error creating issue", e)
+    }
 
-    // let cardResult
-    // try {
-    //   cardResult = await octokit.rest.projects.createCard({
-    //     column_id: task.properties.Status.select
-    //       ? statusMap[task.properties.Status.select.id]
-    //         ? statusMap[task.properties.Status.select.id]
-    //         : columns.data[0].id
-    //       : columns.data[0].id,
-    //     content_id: issueResult.data.id, //1011468002,
-    //     content_type: "Issue",
-    //   })
-    //   console.log("created card", cardResult)
-    // } catch (e) {
-    //   console.log("erro creating card", e)
-    // }
+    let cardResult
+    try {
+      cardResult = await octokit.rest.projects.createCard({
+        column_id: task.properties.Status.select
+          ? statusMap[task.properties.Status.select.id]
+            ? statusMap[task.properties.Status.select.id]
+            : columns.data[0].id
+          : columns.data[0].id,
+        content_id: issueResult.data.id, //1011468002,
+        content_type: "Issue",
+      })
+      console.log("created card", cardResult)
+    } catch (e) {
+      console.log("erro creating card", e)
+    }
 
-    // await addIssueNumberIntoNotionTask(
-    //   task.id,
-    //   issueResult.data.number,
-    //   cardResult.data.id
-    // )
-
-    await addIssueNumberIntoNotionTask(task.id, 50, 70104628)
+    await addIssueNumberIntoNotionTask(
+      task.id,
+      issueResult.data.number,
+      cardResult.data.id
+    )
   })
 }
